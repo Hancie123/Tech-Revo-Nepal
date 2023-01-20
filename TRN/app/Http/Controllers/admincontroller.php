c50a3eabf8d8b94065d9bc8887ba8a201dd6f7e8
Hancie Phago <Hanciewanemphago@gmail.com>
2023-01-20 19:42:14 +0545

    commit change

--- TRN/app/Http/Controllers/admincontroller.php
+++ TRN/app/Http/Controllers/admincontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin;
 
 class admincontroller extends Controller
 {
@@ -14,12 +15,23 @@ class admincontroller extends Controller
     public function registrationcheck(Request $request){
         $request->validate([
             'name'=>'required',
-            'email'=>'required',
+            'email'=>'required|email',
             'mobile'=>'required',
             'password'=>'required',
             'address'=>'required',
             'date'=>'required',
         ]);
+
+        
+        //insert query
+        $admin=new Admin;
+        $admin->name=$request['name'];
+        $admin->dob=$request['date'];
+        $admin->email=$request['email'];
+        $admin->mobileno=$request['mobile'];
+        $admin->address=$request['address'];
+        $admin->password=md5($request['password']);
+        $admin->save();
 
     }
 }
