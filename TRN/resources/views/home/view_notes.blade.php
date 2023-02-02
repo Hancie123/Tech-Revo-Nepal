67c828ff92d9ef237bce43b45290fb6038504c8b
Hancie Phago <Hanciewanemphago@gmail.com>
2023-02-02 11:07:07 +0545

    commit change

--- TRN/resources/views/home/view_notes.blade.php
+++ TRN/resources/views/home/view_notes.blade.php
@@ -393,7 +393,7 @@
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="modeltitle">TRN Note</h1>
+                        <h1 class="modal-title fs-5" id="modeltitle">TRN Notes</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
