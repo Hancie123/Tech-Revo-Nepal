fa7126102a080769d4095164157100c7e7f63ee7
Hancie Phago <Hanciewanemphago@gmail.com>
2023-03-08 23:45:33 +0545

    commit change

--- TRN/resources/views/mail.blade.php
+++ TRN/resources/views/mail.blade.php
@@ -1,5 +1,24 @@
-<tr>
+<!DOCTYPE HTML>
+<html>
 
-    <td>{{$subject}}</td>
-    <td>{{$announcement}}</td>
-</tr>
+<head>
+    <title>Tech Revo Nepal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <h2 class="text-center">Tech Revo Nepal</h2>
+
+    <p>Tech Revo Nepal have new announcement for you.</p>
+
+    <p>Subject: {{$subject}} </p>
+    <p>Announcement: {{$announcement}} </p>
+
+    <br>
+    <p> Contact us at 9825915122 or techrevonepal@gmail.com if you need any additional support.</p>
+    <p>Sincerely,<br>
+        Tech Revo Nepal</p>
+</body>
+
+</html>
